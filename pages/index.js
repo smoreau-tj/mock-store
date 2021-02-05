f4926c7baef11ef0e7e33cd741e3aec1f64b50b1
Serge M <serge@tommyjohn.com>
2021-02-05 09:23:45 -0500

    fixing error in branch

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome Gavin</a>
+          <a>Welcome Gavin</a>
         </h1>
 
         <p className={styles.description}>
